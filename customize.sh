9fcea6c0b864981157b5331abe90eb2699124ab6
iFHax <ifhax@proton.me>
2025-04-14 15:22:42 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -8,3 +8,9 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' openwrt/package/lean/default-
 
 ## Set default language to auto (remove forced language setting)
 sed -i '/config language/d' openwrt/package/lean/default-settings/files/zzz-default-settings
+
+## Set default and permanent TTL to 64
+echo 'net.ipv4.ip_default_ttl=64' >> openwrt/package/base-files/files/etc/sysctl.conf
+
+## Change default SSID to "DCWRT"
+sed -i "s/option ssid.*/option ssid 'DCWRT'/" openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
