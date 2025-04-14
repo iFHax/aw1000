95430ee631649a8c9c24d2dab592b4d707347f5b
iFHax <ifhax@proton.me>
2025-04-14 11:38:41 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-## Change default IP (optional, uncomment if needed)
-## sed -i 's/192.168.1.1/192.168.10.1/g' openwrt/package/base-files/files/bin/config_generate
-
 ## Remove default root password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
 ## Set default theme to Argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' openwrt/package/lean/default-settings/files/zzz-default-settings
+
+## Set default language to auto (remove forced language setting)
+sed -i '/config language/d' openwrt/package/lean/default-settings/files/zzz-default-settings
